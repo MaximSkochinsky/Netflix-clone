3c98ee67d6ec5329e8966b9542c6ed5a9377c683
MaximSkochinsky <m.skochinsky@mail.ru>
2024-01-01 19:23:24 +0300

    Netflix clne

---
+++ components/NavbarItem.tsx
@@ -0,0 +1,14 @@
+interface NavbarItemsProps {
+    label: string;
+}
+
+
+const NavbarItem: React.FC<NavbarItemsProps> = ({label}) => {
+  return (
+    <div className="text-white cursor-pointer hover:text-gray-300 transition">
+        {label}
+    </div>
+  )
+}
+
+export default NavbarItem
---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { getSession, signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import Navbar from "@/components/Navbar";
+import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
+import InfoModal from "@/components/InfoModal";
+import useInfoModal, { ModalStoreInterface } from "@/hooks/useInfoModal";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        pernament: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
+export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const {isOpen, closeModal} = useInfoModal();
+
+  const { data: user } = useCurrentUser();
+
+  return (
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
+    </>
+  );
+}
